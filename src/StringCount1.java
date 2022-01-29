6e47d7bbf063752893c866f756f8c0a5dd10abce
¨Hanna <hannarealkrasik@gmail.com>
2022-01-28 19:08:44 -0600

    add class StringCount1

---
+++ src/StringCount1.java
@@ -0,0 +1,45 @@
+/* Complete the method/function so that it converts dash/underscore
+delimited words into camel casing. The first word within the output should
+be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"*/
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class StringCount1 {
+    public static void main(String[] args) {
+
+         String s = "The_Stealth_Warrior";
+         List<Character> m1 = new ArrayList<>();
+        for (char ch : s.toCharArray()) {
+            m1.add(ch);
+        }
+         for (int i = 0; i < m1.size(); i ++)
+           if ( m1.get(i) == '-' ||  m1.get(i) == '_'){
+           Character r =  m1.get(i+1).charValue();
+                r = Character.toUpperCase(r);
+                m1.set((i+1), r);
+             m1.remove(i);
+           }
+        StringBuilder sb = new StringBuilder();
+        for (Character chr : m1) {
+            sb.append(chr);
+        }
+            System.out.print(sb.toString());
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
