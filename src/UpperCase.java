5ad79866b4caf20dfbf8e0e3a29010c87e99eb13
¨Hanna <hannarealkrasik@gmail.com>
2022-03-18 18:43:01 -0600

    add class StringCount1

---
+++ src/UpperCase.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class UpperCase {
+
+
+        public static String MakeUpperCase(String str) {
+        String Upp = str.toUpperCase(Locale.ROOT);
+return Upp;
+        }
+}
