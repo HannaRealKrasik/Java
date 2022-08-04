03f2837d38d0e8e1db58c30508beeca124313b3f
Hanna Krasik <hkrasik@apexsystems.com>
2022-08-03 19:47:49 -0500

    Change Password done

--- src/apexsystem/Password.java
+++ src/apexsystem/Password.java
@@ -1,31 +1,42 @@
 package apexsystem;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Password {
-    static boolean s = false;
     public static Scanner scanner = new Scanner(System.in);
-    public static boolean myPass(String pass) {
-        if (pass.length() <= 8)
-        return false;
-        for(int i = 0; i < pass.length(); i++)
-            if(!Character.isUpperCase(pass.charAt(i)))
-                return false;
-
-pass.
-    }boolean
+    static char ch;
+   static Pattern special = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+    public static String myPass(String oldPass, String newPass) {
+        for (int i = 0; i < newPass.length(); i++)
+            ch = newPass.charAt(i);
+            if (Character.isUpperCase(ch))
+                System.out.println();
+             else   return "should to be upperCase";
+            if(oldPass.equals(newPass))
+            return "New password can't be as the old password";
+            if (newPass.length() < 8)
+            return "Password can't be less then 8 characters";
+        Matcher p = special.matcher(newPass);
+        if (p.find())
+            System.out.println();
+        else   return "should to be specialCharacter";
+        return "Password changed";
+    }
 
     public static void main(String[] args) {
+        String s;
 
+        do {
+            System.out.println("Enter your old password");
+            String oldPassword = scanner.next();
+            System.out.println("Enter your new password");
+            String newPassword = scanner.next();
+          //  myPass(oldPassword,newPassword);
+            s = myPass(oldPassword,newPassword);
+            System.out.println(s);
+        } while  (s != "Password changed");
+    }
 
-    do{
-       s = false;
-        System.out.println("Enter your password");
-       String password = scanner.toString();
-
-    }while (s);
-
-
-
-}
 }
