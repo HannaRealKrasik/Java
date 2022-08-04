2c439f09d24839269cc8b01d4babce358f187934
Hanna Krasik <hkrasik@apexsystems.com>
2022-08-03 19:56:53 -0500

    Change Password done

--- src/apexsystem/Password.java
+++ src/apexsystem/Password.java
@@ -1,5 +1,6 @@
 package apexsystem;
 
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,21 +8,16 @@ import java.util.regex.Pattern;
 public class Password {
     public static Scanner scanner = new Scanner(System.in);
     static char ch;
-   static Pattern special = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
     public static String myPass(String oldPass, String newPass) {
-        for (int i = 0; i < newPass.length(); i++)
-            ch = newPass.charAt(i);
-            if (Character.isUpperCase(ch))
-                System.out.println();
-             else   return "should to be upperCase";
+            if (newPass.equals(newPass.toLowerCase(Locale.ROOT)))
+               return "should to be upperCase";
             if(oldPass.equals(newPass))
-            return "New password can't be as the old password";
+             return "New password can't be as the old password";
             if (newPass.length() < 8)
-            return "Password can't be less then 8 characters";
-        Matcher p = special.matcher(newPass);
-        if (p.find())
-            System.out.println();
-        else   return "should to be specialCharacter";
+             return "Password can't be less then 8 characters";
+      //  Matcher p = special.matcher(newPass);
+        if (newPass.matches("[a-zA-Z0-9 ]*"))
+           return "should to be specialCharacter";
         return "Password changed";
     }
 
